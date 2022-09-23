03b3f366e5ea424ee09d13a25512e7d06accbec3
Tukudani <zin9349@outlook.com>
2022-09-24 04:30:28 +0900

    URL変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/my-profile/',
+    base: '/',
     build: {
         outDir: 'docs',
         assetsDir: './',
