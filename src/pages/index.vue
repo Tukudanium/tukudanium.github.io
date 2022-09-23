c71fe0a1eac9a791a0c0fdbeee12fa90e2f89213
Tukudani <zin9349@outlook.com>
2022-09-24 05:19:46 +0900

    チカチカ感調整

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -40,10 +40,10 @@ export default {
 }
 @keyframes glitch_effect {
     0% {
-        -webkit-filter: drop-shadow(1px 0px 0.5px #ea36af) drop-shadow(-2px 0px 0.5px #75fa69);
+        -webkit-filter: drop-shadow(1px 0px 0.7px #ea36af) drop-shadow(-2px 0px 0.5px #75fa69);
     }
     100% {
-        -webkit-filter: drop-shadow(2px 0.8px 1.3px #ea36af) drop-shadow(-1.5px -0.5px 2px #75fa69);
+        -webkit-filter: drop-shadow(1.7px 0.8px 1.3px #ea36af) drop-shadow(-1.5px -0.5px 2px #75fa69);
     }
 }
 </style>
