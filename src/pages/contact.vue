8cba3c43e63f03ec9d96d4d2bac5d148d649063b
Tukudani <zin9349@outlook.com>
2022-10-13 23:56:08 +0900

    fix:画面サイズを極端に小さくした際の画面表示の不具合を修正

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="justify-center items-center mt-16 flex w-full h-full">
+    <div class="justify-center items-center mt-16 grid w-full h-full overflow-y-scroll delScroll">
         <div class="h-800">contact</div>
     </div>
 </template>
@@ -24,4 +24,16 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.delScroll {
+    /*IE(Internet Explorer)・Microsoft Edgeへの対応*/
+    -ms-overflow-style: none;
+    /*Firefoxへの対応*/
+    scrollbar-width: none;
+}
+
+/*Google Chrome、Safariへの対応*/
+.delScroll::-webkit-scrollbar {
+    display: none;
+}
+</style>
