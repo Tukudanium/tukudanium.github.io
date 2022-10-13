65bcc4d7d0a6b3c52540aefaf9bdbc8e6353ece5
Tukudani <zin9349@outlook.com>
2022-10-13 23:58:43 +0900

    update:成果物カードを反映

--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -1,27 +1,54 @@
 <template>
-    <div class="justify-center items-center mt-16 flex w-full h-full">
-        <div class="h-800">ahoaho</div>
+    <div class="justify-center items-center mt-16 grid w-full h-full overflow-y-scroll delScroll">
+        <div>
+            <portfolio-card :image-path="'/src/assets/image/dotArt.png'" :title="'DotArt'" :summarys="dotArtSummary" :links="dotArtLinks"></portfolio-card>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { ref } from 'vue'
-
+import portfolioCard from '../components/tailblocks/portfolioCard.vue'
+import { Link } from '../types/link'
 export default {
     name: 'PortfolioPage',
+    components: { portfolioCard },
     setup() {
         const msg = ref('hello')
 
         const changeMsg = () => {
             msg.value = 'world'
         }
-
+        const dotArtSummary: string[] = ['「PCでもスマホでもタブレットでも、好きな時に気軽にドット絵を描けるWEBアプリ」が目標。', '今後アップデート予定。']
+        const dotArtLinks: Link[] = [
+            {
+                text: 'github',
+                url: 'https://github.com/TeamDotArt',
+            },
+            {
+                text: '公開ページ',
+                url: 'https://dotart.riml.work/',
+            },
+        ]
         return {
             msg,
+            dotArtSummary,
+            dotArtLinks,
             changeMsg,
         }
     },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.delScroll {
+    /*IE(Internet Explorer)・Microsoft Edgeへの対応*/
+    -ms-overflow-style: none;
+    /*Firefoxへの対応*/
+    scrollbar-width: none;
+}
+/*Google Chrome、Safariへの対応*/
+.delScroll::-webkit-scrollbar {
+    display: none;
+}
+</style>
