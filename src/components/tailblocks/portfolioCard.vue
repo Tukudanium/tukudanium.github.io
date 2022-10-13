038425bd3e48e784f700c46652ea8382c1dfa904
Tukudani <zin9349@outlook.com>
2022-10-13 23:55:29 +0900

    add:自己紹介、成果物カードコンポーネント作成

---
+++ src/components/tailblocks/portfolioCard.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="w-96 h-96 my-6 bg-gray-300 shadow rounded border border-transparent hover:border-blue-500">
+        <div class="h-2/5 flex">
+            <div class="h-full w-2/5 checker-bg items-center justify-center p-5 text-blue-500">
+                <div class="checker-bg flex items-center justify-center rounded-lg">
+                    <img class="w-28 h-28 bg-gray-100 bg-cover bg-center rounded-lg" :src="imagePath" />
+                </div>
+            </div>
+            <div class="py-5 pr-5 w-3/5">
+                <div class="text-left">
+                    <h1 class="text-gray-900 font-medium text-lg">◆{{ title }}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="h-3/5">
+            <div class="h-3/4 flex flex-col justify-center text-left p-5 pt-2 border-t border-gray-400">
+                <div v-for="summary in summarys" :key="summary">
+                    <h1 class="text-gray-900 font-medium text-base">・{{ summary }}</h1>
+                </div>
+            </div>
+            <div class="h-1/4 text-left p-5 py-1 border-t border-gray-400">
+                <div v-for="link in links" :key="link.text">
+                    <a :href="link.url" class="text-indigo-600 text-sm no-underline hover:underline ...">{{ link.text }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Link } from '../../types/link'
+export default {
+    name: 'PortfolioCard',
+    props: {
+        imagePath: {
+            type: String,
+            default: '',
+        },
+        title: {
+            type: String,
+            default: '',
+        },
+        summarys: {
+            type: Array as () => string[],
+            default: () => [],
+        },
+        links: {
+            type: Array as () => Link[],
+            default: () => [],
+        },
+    },
+    setup(props) {
+        console.log(props.summarys)
+        return {}
+    },
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/tailblocks/profileCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="w-80 my-6 bg-gray-300 shadow rounded border border-transparent hover:border-blue-500">
+        <div class="h-60 w-full checker-bg items-center justify-center p-4 text-blue-500">
+            <div class="w-full checker-bg flex items-center justify-center p-4 text-blue-500">
+                <img class="w-32 h-32 bg-gray-100 rounded-full bg-cover bg-center" :src="iconImagePath" />
+            </div>
+            <h1 class="text-gray-900 font-medium text-lg">佐川 仁庸</h1>
+            <h1 class="text-gray-900 font-medium text-lg">tukudani</h1>
+        </div>
+
+        <div class="p-4 border-t border-gray-400">
+            <div class="flex items-center justify-between">
+                <h1 class="text-gray-900 font-medium text-lg">◆ 経歴</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'ProfileCard',
+    props: {
+        iconImagePath: {
+            type: String,
+            default: '',
+        },
+    },
+    setup() {
+        return {}
+    },
+}
+</script>
+
+<style scoped></style>
