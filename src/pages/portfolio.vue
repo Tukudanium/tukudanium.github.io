aaeb5242e30e02b108090194dbe563c206dd93c8
Tukudani <zin9349@outlook.com>
2022-10-25 03:16:00 +0900

    fix:ページタイトルのミス修正

--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -26,9 +26,9 @@ const hukushimaLinks: Link[] = [
 
 <template>
     <div class="justify-center items-center mt-16 grid w-full h-full overflow-y-scroll delScroll">
-        <div class="object-center w-4/5 overflow-x-scroll mx-auto">
+        <div class="object-center w-4/5 mx-auto">
             <h1 class="text-gray-900 font-medium text-lg">作ったもの</h1>
-            <div class="flex p-0">
+            <div class="flex m-2 p-0 overflow-x-scroll">
                 <portfolioCard :image-path="useRequiredUrl('dotArt')" :title="'DotArt'" :summarys="dotArtSummary" :links="dotArtLinks"></portfolioCard>
                 <portfolioCard :image-path="useRequiredUrl('fukushimaTyping')" :title="'福島タイピング'" :summarys="hukushimatSummary" :links="hukushimaLinks"></portfolioCard>
             </div>
