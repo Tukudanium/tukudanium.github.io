4b8926f4fd97e0c0fb51502bbc845cbe9c6ea882
Tukudani <zin9349@outlook.com>
2022-11-03 04:58:18 +0900

    build

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
 import './style.css'
+import './index.css'
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from "./router"
-import './index.css'
 
 const app = createApp(App)
 app.use(createPinia())
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/tukudanium.github.io/' : './',
-    plugins: [vue()]
+    base: './',
+    build: {
+        outDir: 'docs',
+        assetsDir: './',
+    },
+    plugins: [vue(),pluginRewriteAll()]
 })
