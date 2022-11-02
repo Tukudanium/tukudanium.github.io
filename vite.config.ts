0adc8deac3effd98aa47d83bdc8dfcf22d177cf6
Tukudani <zin9349@outlook.com>
2022-11-03 05:05:53 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +8,5 @@ export default defineConfig({
         outDir: 'docs',
         assetsDir: './',
     },
-    plugins: [vue(),pluginRewriteAll()]
+    plugins: [vue()]
 })
